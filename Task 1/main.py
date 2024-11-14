5512e10e2ab03433f0066592fc5909cb53b66a94
vanshita00 <129608607+vanshita00@users.noreply.github.com>
2024-11-14 12:00:25 +0530

    Add files via upload

---
+++ Task 1/main.py
@@ -0,0 +1,25 @@
+import json
+file_path = r"data_set_python_training.json"
+def count_unit_source(file_path):
+    count_inter=0
+    count_intra=0
+    count_not_defined=0
+    with open(file_path, mode='r') as file:
+        data = json.load(file)
+        for item in data:
+            unit_source=item.get("unit_source")
+            if unit_source=="INTER":
+                count_inter+=1
+            elif unit_source=="INTRA":
+                count_intra+=1
+            else:
+                count_not_defined += 1
+    print("INTER-", count_inter)
+    print("INTRA-", count_intra)
+    print("Not Defined-", count_not_defined)
+    return {
+        "INTER":count_inter,
+        "INTRA":count_intra,
+        "Not Defined":count_not_defined
+    }
+count_unit_source(file_path)
